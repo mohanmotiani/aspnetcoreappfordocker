4e510bfa2fe98897a23b7f90b01559c9a5b46b27
Mohan Motiani <mohan.accusol@gmail.com>
2020-06-05 15:56:51 +0530

    minor change for poc. Docker menu page added

---
+++ Pages/Docker.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetcoreappfordocker.Pages
+{
+    public class DockerModel : PageModel
+    {
+        private readonly ILogger<DockerModel> _logger;
+
+        public DockerModel(ILogger<DockerModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
